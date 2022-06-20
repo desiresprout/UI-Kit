cce9cd7d2312145e99bddaaa243a41b04d2c4844
mara <desiresprout@gmail.com>
2022-06-20 12:45:10 +0900

    Add checkbox defaultChecked

--- components/CheckBox.tsx
+++ components/CheckBox.tsx
@@ -1,26 +1,33 @@
-import React, { FC, ComponentProps, useRef, useCallback } from 'react';
+import React, { FC, ComponentProps, useCallback } from 'react';
 import { styled } from '@utils/stitchesConfig';
 
 export interface CheckBoxProps extends Omit<ComponentProps<'input'>, 'onChange'> {
   label?: string;
   checked?: boolean;
   disabled?: boolean;
+  defaultChecked?: boolean;
   onChange?: (checked: boolean) => void;
 }
 
-const CheckBox: FC<CheckBoxProps> = ({ label, checked, disabled, onChange }) => {
+const CheckBox: FC<CheckBoxProps> = ({ label, checked, disabled, defaultChecked, onChange }) => {
   const handleChange = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
       const checkedValue = e.target.checked;
       onChange?.(checkedValue);
     },
-    [onChange, checked]
+    [onChange]
   );
 
   return (
     <CheckBoxContainer disabled={disabled}>
-      <input type="checkbox" checked={checked} disabled={disabled} onChange={handleChange} />
-      <StyledCheckBox checked={checked} />
+      <input
+        type="checkbox"
+        defaultChecked={defaultChecked}
+        checked={checked}
+        disabled={disabled}
+        onChange={handleChange}
+      />
+      <StyledCheckBox checked={checked} defaultChecked={defaultChecked} />
       <span className="label">{label}</span>
     </CheckBoxContainer>
   );
@@ -35,14 +42,10 @@ const CheckBoxContainer = styled('label', {
   position: 'relative',
   paddingLeft: '15px',
   fontSize: '14px',
-
   input: {
     height: 0,
     zIndex: 1,
     opacity: 0,
-    '&:checked + span': {
-      background: '$primary',
-    },
   },
   '& .label': {
     lineHeight: '1.5',
@@ -74,12 +77,29 @@ const StyledCheckBox = styled('span', {
   width: '20px',
   height: '20px',
   border: '1px solid $disabledBorderColor',
-  '&::after': {
-    display: 'block',
-  },
   variants: {
     checked: {
       true: {
+        background: '$primary',
+        '&::after': {
+          background: '$primary',
+          content: ' ',
+          display: 'block',
+          width: '6px',
+          height: '10px',
+          transform: 'rotate(45deg)',
+          position: 'absolute',
+          left: '6px',
+          top: '2px',
+          border: '2px solid white',
+          borderWidth: '0 2px 2px 0',
+          transition: 'cubic-bezier(0.12,0.4,0.29,1.46) 0.1s',
+        },
+      },
+    },
+    defaultChecked: {
+      true: {
+        background: '$primary',
         '&::after': {
           content: ' ',
           display: 'block',
