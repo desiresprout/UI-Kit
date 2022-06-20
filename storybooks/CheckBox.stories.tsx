182d63e079966431f260857218357e2ddd01a164
mara <desiresprout@gmail.com>
2022-06-20 12:43:10 +0900

    Fix) Checkbox storybook

--- storybooks/CheckBox.stories.tsx
+++ storybooks/CheckBox.stories.tsx
@@ -1,47 +1,25 @@
 import React, { useState } from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
 import CheckBox from '@components/CheckBox';
 
 export default {
   title: 'components/CheckBox',
-  argTypes: {
-    label: {
-      description: 'checkbox label',
-      control: { type: 'text' },
-    },
-    disabled: {
-      options: [true, false],
-      control: { type: 'select' },
-    },
+  parameters: {
+    controls: { hideNoControlsWarning: true },
   },
   component: CheckBox,
-} as ComponentMeta<typeof CheckBox>;
+};
 
-const Template: ComponentStory<typeof CheckBox> = ({ disabled, label, defaultChecked }) => {
-  const [checked, setChecked] = useState(defaultChecked);
+export const CheckBoxTemplate = () => {
+  const [isChecked, setChecked] = useState(true);
 
-  const onChange = (checkedValue: boolean) => {
-    setChecked(checkedValue);
+  const handleChange = (checked: boolean) => {
+    setChecked(checked);
   };
 
-  return <CheckBox checked={checked} disabled={disabled} onChange={onChange} label={label} />;
-};
-
-export const defaultCheckBox = Template.bind({});
-export const checkBox = Template.bind({});
-export const disableCheckBox = Template.bind({});
-
-defaultCheckBox.args = {
-  label: 'checkbox',
-};
-
-checkBox.args = {
-  defaultChecked: true,
-  label: 'checkbox123123123213',
-};
-
-disableCheckBox.args = {
-  checked: true,
-  label: 'checkbox',
-  disabled: true,
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '50px', height: '500px' }}>
+      <CheckBox label={'label'} checked={isChecked} onChange={handleChange} />
+      <CheckBox defaultChecked={isChecked} onChange={handleChange} />
+    </div>
+  );
 };
