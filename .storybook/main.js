f85d4a52db35dc5b6c3a1b414e15118008318bf9
mara <desiresprout@gmail.com>
2022-08-04 12:27:01 +0900

    fix) storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,13 +8,15 @@ module.exports = {
     config.resolve.plugins = [
       new TsconfigPathsPlugin({ configFile: path.resolve(__dirname, '../tsconfig.json') }),
     ];
-    config.resolve.extensions.push('.ts', '.tsx');
     config.resolve.alias = {
+      ...config.resolve.alias,
       '@hooks': path.resolve(__dirname, '../', 'hooks'),
       '@components': path.resolve(__dirname, '../', 'components'),
       '@utils': path.resolve(__dirname, '../', 'utils'),
       '@pages': path.resolve(__dirname, '../', 'pages'),
     };
+    config.resolve.extensions.push('.ts', '.tsx');
+
     return config;
   },
   core: {
