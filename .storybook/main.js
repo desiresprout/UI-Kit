5c3d22744362f3e7461fde4fe7b4dc476c1101a6
mara <desiresprout@gmail.com>
2022-08-04 11:53:56 +0900

    fix) deploy file

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,17 @@
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   stories: ['../storybooks/*.stories.@(ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   webpackFinal: async (config) => {
     config.resolve.plugins = [new TsconfigPathsPlugin()];
+    config.resolve.alias = {
+      '@hooks': path.resolve(__dirname, 'hooks'),
+      '@components': path.resolve(__dirname, 'components'),
+      '@utils': path.resolve(__dirname, 'utils'),
+      '@pages': path.resolve(__dirname, 'pages'),
+    };
     return config;
   },
   core: {
